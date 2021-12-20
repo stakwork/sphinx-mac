a1f95603ba1a69a57d8eea79975223a5e9ca411c
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-12-20 12:46:41 -0300

    Showing error when attempting to add an existing contact

--- com.stakwork.sphinx.desktop/Scenes/Contacts/View Controllers/NewContactViewController.swift
+++ com.stakwork.sphinx.desktop/Scenes/Contacts/View Controllers/NewContactViewController.swift
@@ -181,6 +181,10 @@ class NewContactViewController: NSViewController {
             showErrorAlert(message: "invalid.pubkey".localized)
         } else if nickname.isEmpty || pubkey.isEmpty {
             showErrorAlert(message: "nickname.address.required".localized)
+        } else if contactsService.contacts.contains(where: { $0.publicKey == pubkey }) {
+            showErrorAlert(message: "new.contact.error.alreadyExists".localized)
+        } else if contactsService.chats.contains(where: { $0.conversationContact?.publicKey == pubkey }) {
+            showErrorAlert(message: "new.contact.error.alreadyExists".localized)
         } else {
             contactsService.createContact(nickname: nickname, pubKey: pubkey, routeHint: routeHint, pin: pin, callback: { success in
                 self.loading = false
