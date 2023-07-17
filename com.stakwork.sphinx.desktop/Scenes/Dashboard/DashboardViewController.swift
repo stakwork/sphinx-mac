8ddbb6c3190d54ec303f2b7e771ef895b329cc5f
Tom <tomasalbertotiminskas@gmail.com>
2023-07-17 14:39:10 -0300

    Resizing splitted view when resizing window

--- com.stakwork.sphinx.desktop/Scenes/Dashboard/DashboardViewController.swift
+++ com.stakwork.sphinx.desktop/Scenes/Dashboard/DashboardViewController.swift
@@ -286,6 +286,9 @@ class DashboardViewController: NSViewController {
 
 extension DashboardViewController : NSSplitViewDelegate {
     func splitViewDidResizeSubviews(_ notification: Notification) {
+        newDetailViewController?.view.frame = rightSplittedView.bounds
+        listViewController?.view.frame = leftSplittedView.bounds
+        
         if let window = view.window {
             let (minWidth, _) = getWindowMinWidth(leftColumnVisible: !leftSplittedView.isHidden)
             window.minSize = CGSize(width: minWidth, height: kWindowMinHeight)
