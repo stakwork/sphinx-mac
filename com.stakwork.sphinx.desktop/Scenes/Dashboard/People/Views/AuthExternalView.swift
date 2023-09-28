c3a2878995a77d99291eee100933ac417934e1ee
Oluwatobi Bamidele <teebams49@gmail.com>
2023-09-28 20:43:55 +0100

    update: show a toast message when authorization os complete instead of redirecting

--- com.stakwork.sphinx.desktop/Scenes/Dashboard/People/Views/AuthExternalView.swift
+++ com.stakwork.sphinx.desktop/Scenes/Dashboard/People/Views/AuthExternalView.swift
@@ -159,6 +159,7 @@ class AuthExternalView: CommonModalView, LoadableNib {
                 print(url)
                //NSWorkspace.shared.open(url)
             }
+            messageBubbleHelper.showGenericMessageView(text: "authorization.login".localized, delay: 7, textColor: NSColor.white, backColor: NSColor.Sphinx.PrimaryGreen, backAlpha: 1.0)
         } else {
             messageBubbleHelper.showGenericMessageView(text: "authorization.failed".localized, delay: 5, textColor: NSColor.white, backColor: NSColor.Sphinx.BadgeRed, backAlpha: 1.0)
         }
