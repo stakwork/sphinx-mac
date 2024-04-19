29913b27c3be0aecf15a75ad23be13aac6abc1e6
Korede612 <korede.oko-osi@nytengaletech.com>
2024-04-19 05:45:02 +0100

    feat: right panel navigation vc setup

---
+++ com.stakwork.sphinx.desktop/Scenes/Dashboard/DashboardDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardDetailViewController.swift
+//  Sphinx
+//
+//  Created by Oko-osi Korede on 18/04/2024.
+//  Copyright © 2024 Tomas Timinskas. All rights reserved.
+//
+
+import Cocoa
+
+class DashboardDetailViewController: NSViewController {
+
+    @IBOutlet weak var headerView: DashboardDetailHeaderView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
