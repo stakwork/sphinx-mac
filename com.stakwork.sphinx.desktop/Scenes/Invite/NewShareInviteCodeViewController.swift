5f4c1d0a837083c59d85086ee22a5e5af2a04968
Korede612 <okoosikorede@gmail.com>
2024-07-01 03:30:33 +0100

    update: NewShareInviteCodeViewController clean up

--- com.stakwork.sphinx.desktop/Scenes/Invite/NewShareInviteCodeViewController.swift
+++ com.stakwork.sphinx.desktop/Scenes/Invite/NewShareInviteCodeViewController.swift
@@ -18,25 +18,12 @@ class NewShareInviteCodeViewController: NSViewController {
     @IBOutlet weak var viewTitle: NSTextField!
     @IBOutlet weak var copyButton: CustomButton!
     @IBOutlet weak var profileImageView: AspectFillNSImageView!
-    
-    public enum ViewMode: Int {
-        case Invite = 0
-        case PubKey = 1
-        case TribeQR = 2
-        case RouteHint = 3
-        case Invoice = 4
-    }
-    
-    var viewMode = ViewMode.Invite
     var copiedStrind = "code.copied.clipboard".localized
     var profile: UserContact? = nil
     weak var delegate: NewContactDismissDelegate?
     
     static func instantiate(profile: UserContact, delegate: NewContactDismissDelegate?) -> NewShareInviteCodeViewController {
-//        print("here is the viewmode: \(title)")
         let viewController = StoryboardScene.Invite.newShareInviteCodeViewController.instantiate()
-//        viewController.qrCodeString = qrCodeString
-//        viewController.viewTitleString = title
         viewController.profile = profile
         viewController.delegate = delegate
         viewController.qrCodeString = profile.getAddress() ?? ""
@@ -49,31 +36,9 @@ class NewShareInviteCodeViewController: NSViewController {
         copyButton.cursor = .pointingHand
         qrCodeImageView.image = NSImage.qrCode(from: qrCodeString, size: qrCodeImageView.frame.size)
         inviteCodeLabel.stringValue = qrCodeString
-//        setViewTitle()
         configureProfile()
         view.wantsLayer = true
         view.layer?.backgroundColor = .clear
-    }
-    
-    func setViewTitle() {
-//        viewTitle.stringValue = viewTitleString
-//        switch (viewMode) {
-//        case .Invite:
-//            viewTitle.stringValue = "share.invite.code.upper".localized
-//            copiedStrind = "code.copied.clipboard".localized
-//        case .PubKey:
-//            viewTitle.stringValue = "pubkey.upper".localized
-//            copiedStrind = "pubkey.copied.clipboard".localized
-//        case .TribeQR:
-//            viewTitle.stringValue = "group.link.upper".localized
-//            copiedStrind = "link.copied.clipboard".localized
-//        case .RouteHint:
-//            viewTitle.stringValue = "route.hint".localized.uppercased()
-//            copiedStrind = "route.hint.copied.clipboard".localized
-//        case .Invoice:
-//            viewTitle.stringValue = "invoice".localized.uppercased()
-//            copiedStrind = "invoice.copied.clipboard".localized
-//        }
     }
     
     func configureProfile() {
@@ -86,8 +51,6 @@ class NewShareInviteCodeViewController: NSViewController {
                         return
                     }
                     self.profileImageView.bordered = false
-//                    self.profileImageView.borderWidth = 4
-//                    self.profileImageView.borderColor = NSColor.Sphinx.DashboardSearch.cgColor
                     self.profileImageView.image = image
                 })
             } else {
